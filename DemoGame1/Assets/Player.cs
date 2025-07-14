350854dbeecae7a7c253ac4626d0807fa53e1ef7
trueblue222 <shinieechul2012@naver.com>
2025-07-14 18:32:57 +0900

    DemoGame1

---
+++ DemoGame1/Assets/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime * 100.0f, 0));
+    }
+}
